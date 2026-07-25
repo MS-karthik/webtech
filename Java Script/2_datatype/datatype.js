191a19bf01b4f3068bd4737a0d939631bbe60396
Sundar <sundar@Macbooks-MacBook-Pro.local>
2026-07-25 13:22:34 +0530

    Initial commit

---
+++ Java Script/2_datatype/datatype.js
@@ -0,0 +1,20 @@
+let sname = "sundar";
+console.log(sname);
+console.log(typeof sname);
+
+let gender='male'
+console.log(gender);
+console.log(typeof gender);
+
+let about= `good boy`
+console.log(about);
+console.log(typeof about);
+
+// ! 2.number
+
+let age= 25
+console.log(typeof age);
+
+ 
+
+
