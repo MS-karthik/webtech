4c113e9b7d48b978243559f7ee6121088be5283d
Meenakshi Sundaram <meenakshisundaram32982@gmail.com>
2026-07-29 12:26:02 +0530

    29 jul

---
+++ Java Script/3_math/math.js
@@ -0,0 +1,51 @@
+// maximum number
+
+console.log(Math.max(5, 8, 19, 25, 45));
+
+// minimum number
+
+console.log(Math.min(9, 5, 8, 29, 4, 3));
+
+// !math.sqrt()
+
+console.log(Math.sqrt(100));
+
+// !math.abs
+
+console.log(Math.abs(-20));
+
+// ! math.floor()
+
+console.log(Math.floor(5.3));
+
+// ! math.ceil()
+
+console.log(Math.ceil(4.6));
+
+// ! Math.round
+
+console.log(Math.round(5.4));
+console.log(Math.round(5.7));
+
+// ! math.random
+
+console.log(Math.random());
+
+// ! Generate one number between 0 to 10
+
+console.log(Math.floor(Math.random() * 10));
+
+//  ! generate OTP
+
+let otp = Math.floor(Math.random() * 9000 + 1000);
+
+console.log(otp);
+
+//  ! generate number between 10 to 25
+
+let num = Math.floor(Math.random() * 16 + 10);
+console.log(num);
+
+// ! math.pow
+
+console.log(Math.pow(2, 3));
